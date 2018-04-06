8ba99d234f54b609b06a240b6f3defb52d7eee3e
Unknown <ba.raghu@gmail.com>
2018-04-06 11:22:49 +0530

    Adding the webapplicationf or PCF

---
+++ WebApplication1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace WebApplication1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            IWebHostBuilder builder = new WebHostBuilder();
+
+            builder.UseKestrel()
+                .UseStartup<Startup>()
+                .Build().Run();
+        }
+        
+    }
+}
---
+++ WebApplication1/Startup.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Rewrite;
+using Microsoft.Extensions.DependencyInjection;
+using WireMock.Net.StandAlone;
+using WireMock.Server;
+using WireMock.Settings;
+
+namespace WebApplication1
+{
+    public class Startup
+    {
+        private static bool started = false;
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<IMockService, MockService>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IMockService service)
+        {
+            //var rewrite = new RewriteOptions()
+            //    .AddRedirect("films", "movies")
+            //    .AddRewrite("actors", "stars", true);
+
+            //app.UseRewriter(rewrite);
+
+            //app.Run(async context =>
+            //{
+            //    var path = context.Request.Path;
+            //    var query = context.Request.QueryString;
+            //    await context.Response.WriteAsync($"New URL: {path}{query}");
+            //});
+        }
+    }
+
+    public interface IMockService
+    {
+        void start();
+    }
+
+    public class MockService : IMockService
+    {
+        public void start()
+        {
+
+        }
+
+        public MockService()
+        {
+            StandAloneApp.Start(new FluentMockServerSettings() { StartAdminInterface = true , Port=1111});
+        }
+    }
+}
